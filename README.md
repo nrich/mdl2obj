df1fe52fc60fb943884087c4272b71bfa0a45b31
Florian Zwoch <fzwoch@gmail.com>
2018-12-27 17:08:52 +0100

    reimplement in Go. No GLib required.

---
+++ README.md
@@ -0,0 +1,22 @@
+# mdl2obj
+
+Converts Quake1 model files to Wavefront OBJ model data. It will only
+convert the first frame of a model (in case the model is animated).
+It does not extract model texture data.
+
+## Usage
+
+    mdl2obj <model.mdl>
+
+It will read the input model file and will create the following
+files in the current working directory:
+
+    model.obj
+    model.mtl
+
+The model's material `model.mtl` file expects a texture file named
+`model.jpg` in the same directory.
+
+## Build
+
+    go build mdl2obj.go
---
+++ mdl2obj.go
@@ -0,0 +1,246 @@
+/*
+ * mdl2obj
+ *
+ * Copyright (C) 2016-2018 Florian Zwoch <fzwoch@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package main
+
+import (
+	"bufio"
+	"encoding/binary"
+	"fmt"
+	"html/template"
+	"io"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+type vec3 struct {
+	X float32
+	Y float32
+	Z float32
+}
+
+type mdlHeader struct {
+	ID           uint32
+	Version      uint32
+	Scale        vec3
+	Origin       vec3
+	Radius       float32
+	Offsets      vec3
+	NumSkins     uint32
+	SkinWidth    uint32
+	SkinHeight   uint32
+	NumVerts     uint32
+	NumTriangles uint32
+	NumFrames    uint32
+	SyncType     uint32
+	Flags        uint32
+	Size         float32
+}
+
+type skin struct {
+	Type uint32
+}
+
+type skinGroup struct {
+	NumSkins uint32
+	Time     float32
+}
+
+type stVert struct {
+	OnSeam uint32
+	S      uint32
+	T      uint32
+}
+
+type triangle struct {
+	Front  uint32
+	Vertex [3]uint32
+}
+
+type vert struct {
+	V      [3]uint8
+	Normal uint8
+}
+
+type frame struct {
+	Type uint32
+	Min  vert
+	Max  vert
+	Name [16]uint8
+}
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Println("usage:", os.Args[0], "<input.mdl>")
+		os.Exit(1)
+	}
+
+	f, err := os.Open(os.Args[1])
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	mdlName := filepath.Base(strings.TrimSuffix(os.Args[1], filepath.Ext(os.Args[1])))
+
+	var mdl mdlHeader
+
+	err = binary.Read(f, binary.LittleEndian, &mdl)
+	if err != nil {
+		panic(err)
+	}
+
+	if mdl.ID != 1330660425 {
+		panic("MDL magic " + string(mdl.Version) + " != \"IDPO\"")
+	}
+
+	if mdl.Version != 6 {
+		panic("MDL version " + string(mdl.Version) + " != 6")
+	}
+
+	for i := 0; i < int(mdl.NumSkins); i++ {
+		var skin skin
+		var skinGroup skinGroup
+
+		skinGroup.NumSkins = 1
+
+		binary.Read(f, binary.LittleEndian, &skin)
+
+		if skin.Type != 0 {
+			binary.Read(f, binary.LittleEndian, &skinGroup)
+		}
+
+		f.Seek(int64(skinGroup.NumSkins*mdl.SkinWidth*mdl.SkinHeight), io.SeekCurrent)
+	}
+
+	stverts := make([]stVert, mdl.NumVerts)
+	triangles := make([]triangle, mdl.NumTriangles)
+
+	binary.Read(f, binary.LittleEndian, &stverts)
+	binary.Read(f, binary.LittleEndian, &triangles)
+
+	verts := [][]vert{
+		make([]vert, mdl.NumFrames),
+	}
+
+	for i := range verts {
+		var frame frame
+
+		binary.Read(f, binary.LittleEndian, &frame)
+
+		if frame.Type != 0 {
+			panic("FIXME")
+		}
+
+		verts[i] = make([]vert, mdl.NumVerts)
+		binary.Read(f, binary.LittleEndian, &verts[i])
+	}
+
+	data := struct {
+		Name string
+		V    [][3]float32
+		VT   [][2]float32
+		F    [][6]uint32
+	}{
+		Name: mdlName,
+		V:    make([][3]float32, len(verts[0])),
+		VT:   make([][2]float32, len(stverts)*2),
+		F:    make([][6]uint32, len(triangles)),
+	}
+
+	for i, v := range verts[0] {
+		data.V[i] = [3]float32{
+			mdl.Scale.X*float32(v.V[0]) + mdl.Origin.X,
+			mdl.Scale.Y*float32(v.V[1]) + mdl.Origin.Y,
+			mdl.Scale.Z*float32(v.V[2]) + mdl.Origin.Z,
+		}
+	}
+
+	for i, stvertice := range stverts {
+		data.VT[i] = [2]float32{
+			float32(stvertice.S) / float32(mdl.SkinWidth),
+			float32(1 - float32(stvertice.T)/float32(mdl.SkinHeight)),
+		}
+		data.VT[i+int(mdl.NumVerts)] = [2]float32{
+			(float32(stvertice.S) + float32(mdl.SkinWidth/2)) / float32(mdl.SkinWidth),
+			1 - float32(stvertice.T)/float32(mdl.SkinHeight),
+		}
+	}
+
+	for i, triangle := range triangles {
+		var uv [3]uint32
+
+		for i, vertex := range triangle.Vertex {
+			uv[i] = vertex
+			if triangle.Front == 0 && stverts[vertex].OnSeam != 0 {
+				uv[i] += mdl.NumVerts
+			}
+		}
+
+		data.F[i] = [6]uint32{
+			triangle.Vertex[0] + 1,
+			uv[0] + 1,
+			triangle.Vertex[2] + 1,
+			uv[2] + 1,
+			triangle.Vertex[1] + 1,
+			uv[1] + 1,
+		}
+	}
+
+	o, err := os.Create(mdlName + ".obj")
+	if err != nil {
+		panic(err)
+	}
+	w := bufio.NewWriter(o)
+
+	t, _ := template.New("").Parse(obj)
+	t.Execute(w, data)
+	w.Flush()
+	o.Close()
+
+	o, err = os.Create(mdlName + ".mtl")
+	if err != nil {
+		panic(err)
+	}
+	w = bufio.NewWriter(o)
+
+	t, _ = template.New("").Parse(mtl)
+	t.Execute(w, data)
+	w.Flush()
+	o.Close()
+}
+
+const obj = `o {{ .Name }}
+mtllib {{ .Name }}.mtl
+usemtl {{ .Name }}
+{{range .V}}v {{ index . 0 }} {{ index . 1 }} {{ index . 2 }}
+{{end}}{{range .VT}}vt {{ index . 0 }} {{ index . 1 }}
+{{end}}{{range .F}}f {{ index . 0 }}/{{ index . 1 }} {{ index . 2 }}/{{ index . 3 }} {{ index . 4 }}/{{ index . 5 }}
+{{end}}`
+
+const mtl = `newmtl {{ .Name }}
+Ka 1.000000 1.000000 1.000000
+Kd 1.000000 1.000000 1.000000
+Ks 0.000000 0.000000 0.000000
+Tr 1.000000
+illum 1
+Ns 0.000000
+map_Kd {{ .Name }}.jpg
+`
